a0aa6d35528c5adc207e875cd0c7338ea3973a19
Iyanalmas <99644683+Iyanalmas@users.noreply.github.com>
2023-04-18 15:40:53 +0000

    Pending changes exported from your codespace

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//syntax PHP
+// ini adalah komentar
+/* 
+ini juga komentar
+*/
+
+//STANDAR OUTPUT
+// echo,print mencetak tulisan atau isi variabel
+// print_r khusus digunakan untuk mencetak isi array
+// var_dump untuk melihat isi dari variabel
+
+echo "iyan almas";
+?>
